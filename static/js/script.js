4dfd9f630ee729a87b6eec2d52356677b964d81b
juandarov <juandarov@gmail.com>
2017-03-22 15:13:15 +0100

    add panier.html

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $(".clickProduct").click(function(){
         window.location.href= 'product.html';
     });
+
+    $("#cart").click(function(){
+        window.location.href= 'panier.html';
+    });
 });
